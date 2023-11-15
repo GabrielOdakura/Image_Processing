cd3c7bcc4e05f6e7e2ce8698cddf8a87f7b3ac31
brenex <brenex0000007@gmail.com>
2023-11-15 17:37:55 -0300

    Mudanças na UI

--- src/App.java
+++ src/App.java
@@ -9,27 +9,28 @@ import javax.swing.*;
 public class App {
 
     public static void main(String args[]){
-        String nomeArquivo;
-        String opcaoDesejada;
-        nomeArquivo = JOptionPane.showInputDialog("Insira o nome do arquivo que você deseja transformar!");
-        if(nomeArquivo == null){
-            JOptionPane.showMessageDialog(null, "Fechando o programa!");
-        }else {
-            opcaoDesejada = JOptionPane.showInputDialog("(1) - Grey Scale" +
-                                                      "\n(2) - Black and White" +
-                                                      "\n(3) - Edge Detection" +
-                                                      "\n(4) - Sharpen");
-            if(opcaoDesejada == null){
-                JOptionPane.showMessageDialog(null, "Fechando o programa!");
-            }else{
-                if(opcaoDesejada.equals("1") || opcaoDesejada.equals("2")){
-                    Controller.transformImage(nomeArquivo, opcaoDesejada);
-                }else if(opcaoDesejada.equals("3") || opcaoDesejada.equals("4")){
-                    Controller.transformImage(nomeArquivo, opcaoDesejada);
-                }else {
-                    JOptionPane.showMessageDialog(null, "Escolha uma opção valida!");
-                }
-            }
-        }
+    	new Gui(700,600);
+//        String nomeArquivo;
+//        String opcaoDesejada;
+//        nomeArquivo = JOptionPane.showInputDialog("Insira o nome do arquivo que você deseja transformar!");
+//        if(nomeArquivo == null){
+//            JOptionPane.showMessageDialog(null, "Fechando o programa!");
+//        }else {
+//            opcaoDesejada = JOptionPane.showInputDialog("(1) - Grey Scale" +
+//                                                      "\n(2) - Black and White" +
+//                                                      "\n(3) - Edge Detection" +
+//                                                      "\n(4) - Sharpen");
+//            if(opcaoDesejada == null){
+//                JOptionPane.showMessageDialog(null, "Fechando o programa!");
+//            }else{
+//                if(opcaoDesejada.equals("1") || opcaoDesejada.equals("2")){
+//                    Controller.transformImage(nomeArquivo, opcaoDesejada);
+//                }else if(opcaoDesejada.equals("3") || opcaoDesejada.equals("4")){
+//                    Controller.transformImage(nomeArquivo, opcaoDesejada);
+//                }else {
+//                    JOptionPane.showMessageDialog(null, "Escolha uma opção valida!");
+//                }
+//            }
+//        }
     }
 }
